5a51a9b75d5ed0c821105e9adc45203044712cc0
Nelly Ghazaryan <106973546+NellyGh@users.noreply.github.com>
2023-03-24 23:32:27 +0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter  basename="/instagram">
+      <BrowserRouter  basename="/instagram-app">
         <App />
       </BrowserRouter>
     </PersistGate>
